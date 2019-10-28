2cd91880410d9faf98585fface2266bc27775de9
wangongshen@gmail.com <wangongshen@gmail.com>
2019-10-28 20:12:56 +0800

    删除

--- EFDemo/Program.cs
+++ EFDemo/Program.cs
@@ -18,7 +18,7 @@ namespace EFDemo
             //UserInfo user = new UserInfo();
             //user.Uname = "zs";
             //user.Pwd = "123456";
-            //user.ShowName = "张三1";
+            //user.ShowName = "张三123";
             //dmc.UserInfo.Add(user);
             //dmc.SaveChanges();
             //Console.WriteLine("添加成功");
@@ -40,30 +40,87 @@ namespace EFDemo
             //#endregion
 
 
-            #region 添加 2
-            //添加数据 EF上下文要对上面的实体做插入操作，要添加对EntityFramework.dll和EntityFramework.SqlServer.dll的引用
-            DataModelContainer dmc = new DataModelContainer();
-            UserInfo user = new UserInfo();
-            user.Uname = "zs";
-            user.Pwd = "123456";
-            user.ShowName = "张三3";
-            dmc.UserInfo.Add(user);
+            //#region 添加 3
+            ////添加数据 EF上下文要对上面的实体做插入操作，要添加对EntityFramework.dll和EntityFramework.SqlServer.dll的引用
+            //DataModelContainer dmc = new DataModelContainer();
+            //UserInfo user = new UserInfo();
+            //user.Uname = "zs";
+            //user.Pwd = "123456";
+            //user.ShowName = "张三3";
+            //dmc.UserInfo.Add(user);
 
-            OrderInfo order1 = new OrderInfo();
-            order1.Content = "满100元";
-            dmc.OrderInfo.Add(order1);
+            //OrderInfo order1 = new OrderInfo();
+            //order1.Content = "满100元";
+            //dmc.OrderInfo.Add(order1);
 
-            OrderInfo order2 = new OrderInfo();
-            order2.Content = "满100元";
-            dmc.OrderInfo.Add(order2);
+            //OrderInfo order2 = new OrderInfo();
+            //order2.Content = "满100元";
+            //dmc.OrderInfo.Add(order2);
 
-            user.OrderInfo.Add(order1);
-            user.OrderInfo.Add(order2);
+            //user.OrderInfo.Add(order1);
+            //user.OrderInfo.Add(order2);
 
-            dmc.SaveChanges();
-            Console.WriteLine("添加成功");
-            Console.ReadKey();
-            #endregion
+            //dmc.SaveChanges();
+            //Console.WriteLine("添加成功");
+            //Console.ReadKey();
+            //#endregion
+
+            //#region 修改全部字段 1
+            ////添加数据 EF上下文要对上面的实体做插入操作，要添加对EntityFramework.dll和EntityFramework.SqlServer.dll的引用
+            //DataModelContainer dmc = new DataModelContainer();
+            //UserInfo user = new UserInfo();
+            //user.Id = 1;
+            //user.Uname = "zs1";
+            //user.Pwd = "123456";
+            //user.ShowName = "张三3";
+            ////泛型写法 修改所有字段
+            //dmc.Entry<UserInfo>(user).State = EntityState.Modified;
+
+            //dmc.SaveChanges();
+            //Console.WriteLine("修改成功");
+            //Console.ReadKey();
+            //#endregion
+
+            //#region 修改部分字段 1
+            ////添加数据 EF上下文要对上面的实体做插入操作，要添加对EntityFramework.dll和EntityFramework.SqlServer.dll的引用
+            //DataModelContainer dmc = new DataModelContainer();
+            //UserInfo user = new UserInfo();
+            //user.Id = 1;
+            //user.Uname = "zs12";
+            //user.Pwd = "1234568888";
+            //user.ShowName = "张三31";
+            ////泛型写法 修改所有字段
+            //dmc.UserInfo.Attach(user);
+            //dmc.Entry<UserInfo>(user).Property("Pwd").IsModified = true;
+            //dmc.Entry<UserInfo>(user).Property("ShowName").IsModified = true;
+            //dmc.SaveChanges();
+            //Console.WriteLine("修改成功");
+            //Console.ReadKey();
+            //#endregion
+
+            //#region 删除 1
+            ////添加数据 EF上下文要对上面的实体做插入操作，要添加对EntityFramework.dll和EntityFramework.SqlServer.dll的引用
+            //DataModelContainer dmc = new DataModelContainer();
+            //UserInfo user = new UserInfo();
+            //user.Id = 2;
+            //dmc.Entry<UserInfo>(user).State = EntityState.Deleted;
+            //dmc.SaveChanges();
+
+            //Console.WriteLine("删除成功");
+            //Console.ReadKey();
+            //#endregion
+
+            //#region 删除 1
+            ////添加数据 EF上下文要对上面的实体做插入操作，要添加对EntityFramework.dll和EntityFramework.SqlServer.dll的引用
+            //DataModelContainer dmc = new DataModelContainer();
+            //UserInfo user = new UserInfo();
+            //user.Id = 3;
+            //dmc.UserInfo.Attach(user);
+            //dmc.UserInfo.Remove(user);
+            //dmc.SaveChanges();
+            //Console.WriteLine("删除成功");
+            //Console.ReadKey();
+            //#endregion
 
 
         }
