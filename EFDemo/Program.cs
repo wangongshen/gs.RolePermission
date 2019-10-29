8bcd190280e1baf3cdd2f54d020b97b44353016d
wangongshen@gmail.com <wangongshen@gmail.com>
2019-10-29 10:20:28 +0800

    多表查询

--- EFDemo/Program.cs
+++ EFDemo/Program.cs
@@ -146,12 +146,50 @@ namespace EFDemo
             //Console.ReadKey();
             //#endregion
 
-            #region foreach遍历
-            DataModelContainer dmc = new DataModelContainer();
-            var data = from u in dmc.UserInfo where u.Id < 6 && u.Uname.StartsWith("z") select u;
-            foreach (var user in data)
+            //#region foreach遍历
+            //DataModelContainer dmc = new DataModelContainer();
+            //var data = from u in dmc.UserInfo where u.Id < 6 && u.Uname.StartsWith("z") select u;
+            //foreach (var user in data)
+            //{
+            //    Console.WriteLine("编号："+user.Id+"/n姓名：" + user.Uname);
+            //}
+            //Console.ReadKey();
+            //#endregion
+
+            //#region 延迟加载，用到的时候才去查询数据库
+            //DataModelContainer dbCotent = new DataModelContainer();
+
+            //var temp1 = from u in dbCotent.UserInfo
+            //            where u.Id < 15
+            //            select u;
+
+            //var temp2 = from u in temp1
+            //            where u.Id < 10 && u.Uname.Contains("z")
+            //            select u;
+
+            ////注意：上面有2个Linq to EF语句，但不会执行2次查询，因为都会延迟到用的时候才查询，所以上面2条查询会合并后才执行查询
+
+            //foreach (var user in temp2)
+            //{
+            //    Console.WriteLine(user.Id + "  " + user.Uname);
+            //}
+            //Console.ReadKey();
+            //#endregion
+
+
+            #region 多表查询,通过Include方法取消延迟加载，进行表连接查询，数据量小的时候采用
+            DataModelContainer dbCotent = new DataModelContainer();
+            //用Include即进行了表连接查询，取消了延迟加载
+            var temp = from u in dbCotent.UserInfo.Include("OrderInfo")
+                       where u.Id < 15
+                       select u;
+            //通过导航属性多表查询,没有进行表连接查询
+            foreach (var user in temp)
             {
-                Console.WriteLine("编号："+user.Id+"/n姓名：" + user.Uname);
+                foreach (var orderInfo in user.OrderInfo)
+                {
+                    Console.WriteLine(user.Uname + "  " + orderInfo.Id + "  " + orderInfo.Content);
+                }
             }
             Console.ReadKey();
             #endregion
