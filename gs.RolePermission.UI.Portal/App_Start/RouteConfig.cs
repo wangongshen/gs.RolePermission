4dc4d2fa61214060d58e7d3b71061494c9db2bfb
wangongshen@gmail.com <wangongshen@gmail.com>
2019-11-26 13:45:39 +0800

    模仿win桌面【50%】

--- gs.RolePermission.UI.Portal/App_Start/RouteConfig.cs
+++ gs.RolePermission.UI.Portal/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace gs.RolePermission.UI.Portal
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "UserLogin", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ gs.RolePermission.UI.Portal/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gs.RolePermission.UI.Portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
