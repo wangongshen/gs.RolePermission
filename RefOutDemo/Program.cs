caaff2b793469353ff44a2a0d2684e4dce5413ed
wangongshen@gmail.com <wangongshen@gmail.com>
2019-12-13 16:31:31 +0800

    。

--- RefOutDemo/Program.cs
+++ RefOutDemo/Program.cs
@@ -6,33 +6,48 @@ using System.Threading.Tasks;
 
 namespace RefOutDemo
 {
-    class Program
+    static class Program
     {
-        public readonly string bb;
+        //public readonly string bb;
 
-        Program() {
-            bb = "eqwe";
-            bb = "11";
-            Console.WriteLine("bb:"+ bb);
-        }
+
         static void Main(string[] args)
         {
-            Program program = new Program();
-            int a =3;
-       
-            int b = 5; 
-            Swap(a, b); 
-            Console.WriteLine("a={0} b={1}", a, b);
+            //Program program = new Program();
+            //int a =3;
+
+            //int b = 5; 
+            //Swap(a, b); 
+            //Console.WriteLine("a={0} b={1}", a, b);
+
+
+
+            int num = 2;
+            Console.WriteLine("num1:" + num);
+            num =num.GetT();
+            Console.WriteLine("num2:"+num);
             Console.ReadKey();
 
         }
 
-        static void Swap(int a, int b) {
-            Console.WriteLine(a+b);
-            int t = 0;
-            t = a; 
-            a = b; 
-            b = t; 
+        #region 扩展方式
+        public static int GetT(this int num)
+        {
+            //计算值的2倍
+            return 2 * num;
         }
+        #endregion
+
+        #region ref和out
+        static void Swap(int a, int b)
+        {
+            Console.WriteLine(a + b);
+            int t = 0;
+            t = a;
+            a = b;
+            b = t;
+        }
+        #endregion
+
     }
 }
