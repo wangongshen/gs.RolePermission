5ff39ae40b6a77b6cf1a9ac7cd5c2aa799ef3409
wangongshen@gmail.com <wangongshen@gmail.com>
2019-12-28 17:41:09 +0800

    添加R_Userinfo_Action。。。

---
+++ gs.RolePermission.IBLL/IR_UserInfo_ActionInfoBll.cs
@@ -0,0 +1,13 @@
+﻿using gs.RolePermission.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gs.RolePermission.IBLL
+{
+    public interface IR_UserInfo_ActionInfoBll : IBaseBll<R_UserInfo_ActionInfo>
+    {
+    }
+}
